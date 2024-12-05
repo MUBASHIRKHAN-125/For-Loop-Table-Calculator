c4cb023cabcd07b0a0f210841688f4b354628e99
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2024-12-05 14:33:52 +0500

    css added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function  card()
 
     for(let i=1; i <= myLimitNumber; i++)
     {
-     output += `${myEnterNumber} * ${i} = ${myEnterNumber * i} <br>`;
+     output += `<tr><td>${myEnterNumber}</td><td> * </td><td>${i}</td><td>=</td><td>${myEnterNumber * i}</td> <br>`;
     }
     document.getElementById("maintableoutput").innerHTML = output;
 }
